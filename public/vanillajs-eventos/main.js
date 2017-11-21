484e2bb7575567242788b591fdb4d56545410454
Alumno02 J601 <a02j601@campustls.local>
2017-11-21 12:54:35 -0500

    Agregar vanilla

---
+++ public/vanillajs-eventos/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Estoy listo!!");
+
+    var form = document.querySelectorAll("form");
+
+    form[0].addEventListener("submit", function(event){
+      event.preventDefault();
+      console.log("Enviando");
+    });
+
+    console.log(form);
+})
